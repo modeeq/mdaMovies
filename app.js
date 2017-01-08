d6e411eeb63b05cfbe090b06e92c777b72a5261c
m ali <modeeqali1@gmail.comi>
2017-01-08 14:29:17 -0500

    add file structure

---
+++ app.js
@@ -0,0 +1,19 @@
+var   bodyParser   =  require('body-parser'),
+	  express 	   =  require('express'),
+	  home         =  require('./routes/home');
+
+
+
+var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+
+app.use('/',home);
+
+
+
+module.exports = app;
+app.listen(3000,function(){
+	console.log('port 3000 is runing');
+});
+

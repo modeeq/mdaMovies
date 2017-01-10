984690c8e65a85c4cc943ffe08856a24e37143f0
m ali <modeeqali1@gmail.comi>
2017-01-09 23:03:36 -0500

    add trailer route and update the home layout

--- public/js/home.js
+++ public/js/home.js
@@ -5,41 +5,18 @@ var  close 		 = document.querySelector('#close'),
 	 sections    = document.querySelectorAll('.section'),
  	 watch       = document.querySelectorAll('.movie_trailer');
  
-
  
-close.addEventListener('click',hideVideo);
-function hideVideo(){
-close.style.visibility   = "hidden";
-trailer.style.visibility = "hidden";
-// iframe.parentNode.removeChild(iframe);
-
-nav.style.opacity = 1;
-sections.forEach(function(section){
-section.style.opacity = 1;
-
-});
- 
- } 
 watch.forEach(function(watch){
+	watch.addEventListener('click',function(){
+   var url = watch.childNodes[1].href;
 
- watch.addEventListener('click',function(){
- 
-close.style.visibility   = "block";
-trailer.style.visibility = "block";
-  var link = watch.childNodes[1].href;
-console.log(link);
-document.querySelector('#ytplayer').src=link;
-nav.style.opacity = 0;
-sections.forEach(function(section){
-section.style.opacity = 0;
-
-});
+		window.location.replace(url);
  
  });
 
    
 });
-  
 
+ 
  
  
--- routes/home.js
+++ routes/home.js
@@ -6,5 +6,8 @@ var router  = express.Router();
 router.get('/',function(req, res){
 	res.render('home');
 });
+router.get('/trailer/:url',function(req, res){
+	res.render('trailer',{url:req.params.url});
+});
 
 module.exports = router;

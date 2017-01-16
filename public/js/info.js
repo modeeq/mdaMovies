fff052666089665e1b132dcc5a12d5f8ce331ff4
modeeq <modeeqali1@gmail.com>
2017-01-15 20:24:13 -0500

    add info page

---
+++ public/js/info.js
@@ -0,0 +1,6 @@
+close.addEventListener('click',function(){
+	 window.location.replace('http://localhost:3000');
+
+
+ });
+   
---
+++ routes/info.js
@@ -0,0 +1,25 @@
+var   express     = require('express'),
+	  request     = require('request'),
+	  api         = require('../movieApi');
+
+
+var router  = express.Router();
+ 
+router.get('/:id',function(req, res){
+     api.search_movie(req.params.id+'/recommendations');
+	 api.movie_details(req.params.id,'info');
+
+	 var interval = setInterval(function(){
+     				if (api.Info.length >=1) {
+     					 clearInterval(interval);
+     				     res.render('info',{movie:api.Info,related:api.Related});
+       				}
+     				else{
+     						console.log('___Loading ... ___');
+      					}
+     		},1);
+
+
+ });
+
+module.exports = router;
